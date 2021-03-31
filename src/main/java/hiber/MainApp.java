de9fdba6206bf718bb683551699a53eed735305d
RoGilgan <rogilgan@bk.ru>
2021-03-31 16:33:53 +0200

    Initial commit

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,38 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        UserService userService = context.getBean(UserService.class);
+
+        userService.add(new User("User1", "Lastname1", "user1@mail.ru",
+                new Car("BMW", 5)));
+        userService.add(new User("User2", "Lastname2", "user2@mail.ru",
+                new Car("Mazda", 3)));
+
+        System.out.println(userService.getUserByCar("BMW", 5));
+
+        List<User> users = userService.listUsers();
+        for (User user : users) {
+            System.out.println("Id = " + user.getId());
+            System.out.println("First Name = " + user.getFirstName());
+            System.out.println("Last Name = " + user.getLastName());
+            System.out.println("Email = " + user.getEmail());
+            System.out.println("Model car = " + user.getCar().getModel());
+            System.out.println("Series car = " + user.getCar().getSeries());
+            System.out.println();
+        }
+        context.close();
+    }
+}
---
+++ src/main/java/hiber/model/User.java
@@ -0,0 +1,86 @@
+package hiber.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class User {
+
+   @OneToOne(cascade = CascadeType.ALL)
+   private Car car;
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
+
+   @Column
+   private String firstName;
+
+   @Column
+   private String lastName;
+
+   @Column
+   private String email;
+
+   public User() {}
+   
+   public User(String firstName, String lastName, String email, Car car) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.email = email;
+      this.car = car;
+   }
+
+   public Long getId() {
+      return id;
+   }
+
+   public void setId(Long id) {
+      this.id = id;
+   }
+
+   public String getFirstName() {
+      return firstName;
+   }
+
+   public void setFirstName(String firstName) {
+      this.firstName = firstName;
+   }
+
+   public String getLastName() {
+      return lastName;
+   }
+
+   public void setLastName(String lastName) {
+      this.lastName = lastName;
+   }
+
+   public String getEmail() {
+      return email;
+   }
+
+   public void setEmail(String email) {
+      this.email = email;
+   }
+
+   public Car getCar() {
+      return car;
+   }
+
+   @Override
+   public String toString() {
+      return "\nUser{" +
+              "id=" + id +
+              ", firstName='" + firstName + '\'' +
+              ", lastName='" + lastName + '\'' +
+              ", email='" + email + '\''+
+              '}';
+   }
+}
